a29508cbf062a35d50e13f2e2035086afd9dd708
YJ Soon <something@yjsoon.com>
2023-04-28 12:36:36 +0800

    Added todo model and showed it on ContentView with a List

--- Todo App/ContentView.swift
+++ Todo App/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var todos = [
+        Todo(title: "Feed the cat"),
+        Todo(title: "Buy groceries"),
+        Todo(title: "Finish project"),
+        Todo(title: "Call mom", isCompleted: true),
+        Todo(title: "Go for a run"),
+        Todo(title: "Read a book")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(todos) { todo in
+            Text(todo.title)
         }
-        .padding()
     }
 }
 
---
+++ Todo App/Todo.swift
@@ -0,0 +1,16 @@
+//
+//  Todo.swift
+//  Todo App
+//
+//  Created by YJ Soon on 28/4/23.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id = UUID() // 500716d5-ea3f-4b9b-8954-e35204a67c39 - 2.71 x 10^18
+    var title: String
+    var isCompleted = false
+    
+}
